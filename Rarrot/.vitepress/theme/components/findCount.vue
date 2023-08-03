53fa05b8c6ea5b7f93db698ed4475574350df766
hr121 <568127468@qq.com>
2023-08-03 17:10:19 +0800

    添加计数器

---
+++ Rarrot/.vitepress/theme/components/findCount.vue
@@ -0,0 +1,16 @@
+<template>
+    <p align="right">
+        <img class="inverted-image" src="https://profile-counter.glitch.me/rorrot.cc/count.svg" />
+    </p>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+</script>
+<style scoped>
+.inverted-image {
+    filter: invert(var(--vp-inverted-image-filter));
+}
+
+
+</style>
--- Rarrot/.vitepress/theme/index.js
+++ Rarrot/.vitepress/theme/index.js
@@ -4,6 +4,8 @@ import Theme from 'vitepress/theme'
 import './style.css'
 import containers from './components/container.vue'
 import homeBackground from './components/homeBackground.vue'
+import test from './components/test.vue'
+import findCount from './components/findCount.vue'
 
 export default {
   ...Theme,
@@ -11,12 +13,14 @@ export default {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
 			"doc-after": () => h(containers),
+      "doc-footer-before":()=>h(findCount)
     })
   },
   enhanceApp({ app, router, siteData }) {
     app
       .component('containers', containers)
       .component('homeBackground', homeBackground)
-
+      .component('test', test)
+      .component('findCount',findCount)
   }
 }
