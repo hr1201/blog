b3951050fcbb40fe95ea535329a7ed1871af327f
hr1201 <568127468@qq.com>
2023-12-09 15:52:57 +0800

    新增文章

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 set -e
 
 # 构建
-pnpm run Rarrot:build
+# pnpm run Rarrot:build
 
 # 进入待发布的目录
-cd Rarrot/.vitepress/dist
+# cd Rarrot/.vitepress/dist
 
-git init
+# git init
 git add -A
-git commit -m 'deploy'
+git commit -m '新增文章'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f https://github.com/hr1201/hr1201.github.io.git master
+git push -f https://github.com/hr1201/hr1201.github.io.git main
 
 cd -
